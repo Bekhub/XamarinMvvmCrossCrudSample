b242545b98a6f53d0b03a00301c65f136cdd73b4
Bek <Bek.Go@outlook.com>
2018-07-24 14:34:04 +0200

    Renamed platfrom folders

---
+++ Droid/Views/:Users:Bek:Projects:MvvmCrossCrudSample:Droid:Views:MainView.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Platforms.Android.Views;
+using MvvmCross.Views;
+
+namespace MvvmCrossCrudSample.Droid.Views
+{
+    [MvxActivityPresentation]
+  [Activity(Label = "View for HomeViewModel")]
+  public class HomeView : MvxActivity
+  {
+    protected override void OnCreate(Bundle bundle)
+    {
+      base.OnCreate(bundle);
+      SetContentView(Resource.Layout.MainView);
+    }
+  }
+}
