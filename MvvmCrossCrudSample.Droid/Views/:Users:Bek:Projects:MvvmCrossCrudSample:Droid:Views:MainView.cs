81e836cbb1b393a1bf99ed5ba1c29f21c14f2671
Bek <Bek.Go@outlook.com>
2018-07-24 14:50:44 +0200

    removed old folders

--- MvvmCrossCrudSample.Droid/Views/:Users:Bek:Projects:MvvmCrossCrudSample:Droid:Views:MainView.cs
+++ MvvmCrossCrudSample.Droid/Views/:Users:Bek:Projects:MvvmCrossCrudSample:Droid:Views:MainView.cs
@@ -1,21 +1,25 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Util;
+using Android.Views;
 using Android.Widget;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.Platforms.Android.Views;
+using MvvmCross.Views;
 
 namespace MvvmCrossCrudSample.Droid.Views
 {
     [MvxActivityPresentation]
   [Activity(Label = "View for HomeViewModel")]
-  public class MainView : MvxActivity
+  public class HomeView : MvxActivity
   {
     protected override void OnCreate(Bundle bundle)
     {
